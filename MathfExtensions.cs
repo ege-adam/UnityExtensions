cd8246519b94952541565a70647f2f328434f1e4
Ege <17130579+ege-adam@users.noreply.github.com>
2022-09-07 14:59:39 +0300

    Update MathfExtensions.cs

--- MathfExtensions.cs
+++ MathfExtensions.cs
@@ -6,7 +6,7 @@ namespace Utils
 {
     public static class MathfExtensions
     {
-        public static float Map(this Mathf mathf, float x, float in_min, float in_max, float out_min, float out_max, bool clamp = false)
+        public static float Map(float x, float in_min, float in_max, float out_min, float out_max, bool clamp = false)
         {
             if (clamp) x = Mathf.Max(in_min, Mathf.Min(x, in_max));
             return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
